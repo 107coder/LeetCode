76d14a9186b1d08f29f05e78a24d3db68f93b518
107coder <1493255807@qq.com>
2019-09-07 00:03:45 +0800

    update function2

--- 牛客网/1二位数组的查找.php
+++ 牛客网/1二位数组的查找.php
@@ -12,6 +12,38 @@ function Find($target, $array)
     return false;
 }  
 
+/*
+ * 讨论区的算法思想
+ * 算法思想：
+ * 从左下角的元素开始比较；
+ * 如果target大于该元素，向右移动
+ * 如果target小于该元素，向上移动
+ */
+function Find_2($target, $array)
+{
+  //求出二维矩阵的列数和行数
+  $len1 = count($array);
+  $len2 = count($array[0]);
+
+  $x = $len1-1;
+  $y = 0;
+  while($x>=0 && $y<$len2)
+  {
+    if($target == $array[$x][$y])
+    {
+      return true;
+    }
+    else if($target < $array[$x][$y])
+    {
+      $x--;
+    }
+    else if($target > $array[$x][$y])
+    {
+      $y++;
+    }
+  }
+  return false;
+}
 $array = [
   			[1,2,3,4,5],
   			[6,7,8,9,10],
@@ -19,5 +51,9 @@ $array = [
   			[16,17,18,19,20],
   			[21,22,23,24,25]
   		 ];
-$target = 13;
+$target = 33;
 Find($target,$array);
+
+
+$r = Find_2($target,$array);
+var_dump($r);
