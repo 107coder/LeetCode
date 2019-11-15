b4f55c52fc7335629a5b22401a85d6884dcee901
107coder <1493255807@qq.com>
2019-11-15 17:05:37 +0800

    add 34.35

--- 牛客网/34第一次只出现一次的字符串.php
+++ 牛客网/34第一次只出现一次的字符串.php
@@ -6,4 +6,49 @@
 function FirstNotRepeatingChar($str)
 {
     // write code here
+    if($str == "" || $str==null || strlen($str)==0)
+    {
+    	return -1;
+    }
+
+    for($i=0; $i<strlen($str); $i++)
+    {
+    	if(!isset($str_arr[$str[$i]]))
+    	{
+    		$str_arr[$str[$i]]=1;
+    	}else
+    	{
+    		$str_arr[$str[$i]]++;
+    	}
+    }
+
+    $char = null;
+    foreach ($str_arr as $key => $value) {
+    	if($value == 1)
+    		{
+    			$char = $key;
+    			break;
+    		} 
+    }
+
+    if($char == null)
+    {
+    	return -1;
+    }
+    else
+    {
+	    for($i=0; $i<strlen($str); $i++)
+	    {
+	    	if($str[$i]==$char)
+	    		return $i;
+	    }
+    }
+
+    // print_r($str_arr);
 }
+
+$str = 'GadndfajlgjfdaslgjaHdsajlgdfndsafkldsakg';
+
+$result = FirstNotRepeatingChar($str);
+
+var_dump($result);
