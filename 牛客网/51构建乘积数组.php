6d4d3cf34301a63c864fab02139e99da4f31199e
107coder <1493255807@qq.com>
2019-12-16 20:42:20 +0800

    add 52

--- 牛客网/51构建乘积数组.php
+++ 牛客网/51构建乘积数组.php
@@ -5,5 +5,43 @@
  */
 function multiply($numbers)
 {
-    // write code here
+    $len = count($numbers);
+    for($j=0; $j<$len; $j++){
+    	if($j == 0){
+    		$B[$j] = 1;
+    	}else{
+			$B[$j] = $B[$j-1]*$numbers[$j-1];
+    	}
+    }
+    $temp = 1;
+     for($i=$len-1; $i>=0; $i--){
+    	$B[$i] *= $temp;
+    	$temp *= $numbers[$i];
+    }
+    return $B;
 }
+
+// 遍历的方法
+function test($numbers){
+	$B = [];
+	foreach ($numbers as $key => $value) {
+		$B[$key] = 1;
+		foreach ($numbers as $k => $v) {
+			if($key == $k){
+				continue;
+			}
+			$B[$key] *= $v;
+		}
+	}
+	return $B;
+}
+
+$numbers = [1,2,3,4,5];
+
+$res = test($numbers);
+
+$res = multiply($numbers);
+
+print_r($res);
+
+
