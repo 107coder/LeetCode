70cb4df281284c46b902b2b2dafce0489209d855
107coder <1493255807@qq.com>
2019-12-18 14:05:33 +0800

    52

--- 牛客网/52正则表达式匹配.php
+++ 牛客网/52正则表达式匹配.php
@@ -8,6 +8,52 @@ function match($s, $pattern)
     // write code here
 }
 
+/*
+ 我的基本思路：
+ 	根据字符串s去匹配：分一下情况
+ 	1. 如果检测到，pattern 的对应位置为'.' 直接s 和p 均 进行下一位匹配
+ 	2. 如果匹配到，不相同，检测下一位的值。如果不是* 直接返回false；如果是* 当做匹配0次处理，pattern跳过两个值，继续匹配
+ 	3. 如果检测到相等，直接两个均向下一位继续匹配；
+ 	4. 匹配到对应的位置为* 这时候也需要分不同的情况判断
+ 		1）如果前面的一位仍然为* （不知道是否存在这个情况，暂时这么处理吧）,直接在pattern跳到下一位，s上保持在原地
+ 		2）如果前面的一位是
+*/
 function myFun($s,$pattern){
-	
+	$sLen = strlen($s);
+	$pLen = strlen($pattern);
+	$j=0;
+	$i=0;
+	while($i<$sLen){
+		echo $pattern[$j].$j.PHP_EOL;
+		if($pattern[$j] == '.'){
+			$j++;
+			$i++;
+		}else if($pattern[$j] == '*'){
+			$j++;
+		}else if($s[$i] != $pattern[$j]){
+			if($pattern[$j+1] == '*'){
+				$j += 2;
+			}else{
+				return false;
+			}
+		}else{
+			$j++;
+			$i++;
+		}
+	}
+
+	echo "j=$j".PHP_EOL;
+
+	if($j < $pLen-1){
+		return false;
+	}else{
+		return true;
+	}
 }
+
+$s = 'aaa';
+$pattern = 'a.a';
+$pattern = 'ab*ac*ad';
+
+$res = myFun($s,$pattern);
+var_dump($res);
